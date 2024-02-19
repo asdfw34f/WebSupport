41bd082198347b3f43d4e295be0bfbff7da6183a
Daniil <daniil2002.d11@gmail.com>
2024-02-19 12:20:48 +0300

    Добавьте файлы проекта.

---
+++ Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebSupport.Models;
+
+namespace WebSupport.Controllers.Authentication
+{
+    public class AuthenticationController : Controller
+    {
+
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
